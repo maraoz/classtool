ed60b40f1daa4b6aaf4d6f80cd3124df0c70c5ef
Stephen Pair <stephen@bitpay.com>
2013-03-27 10:01:01 -0400

    a few major improvements

--- README.md
+++ README.md
@@ -5,23 +5,20 @@ This is a simple tool to enable class like idioms in JavaScript.  It is notably 
 from traditional class based inheritance in the following ways:
 
 - inheritance structure is not part of the class definition
-- bindings to the outside are explicitly declared
+- bindings to the outside are explicit (including binding to a superclass)
 - default bindings allow for easy class creation while preserving override capability
 
 The following is an example class definition:
 
-    function ClassSpec() {
-      var fs; // example of a binding
-   
-      this.bindings = function(b) {
-        fs = b.fs || require('fs');
-      };
+    function ClassSpec(b) {
+      var fs = b.fs || require('fs'); // example of a binding
    
       function Class() {
         // Body of constructor function
       };
-      this.class = Class;
-   
+      // Optional - Define a default (but overridable) superclass
+      Class.superclass = b.superclass || require('./superClass').class();   
+
       Class.prototype.methodA = function() {
         // Body of method A
       };
@@ -29,39 +26,68 @@ The following is an example class definition:
       Class.prototype.methodB = function() {
         // Body of method B
       };
+
+      return Class;
     };
     module.defineClass(ClassSpec); 
 
 There are two ways to instantiate the class in client code:
 
     var MyClass = require('./path/to/classfile').class();
-    var MyClass = require('./path/to/classfile').loadClass({fs: mockFileSystem});
+    var MyClass = require('./path/to/classfile').createClass({fs: mockFileSystem});
 
-In the first example, each call will return the same class instance.  In the second
-example, each call returns a new class instance.  The second call also shows how you
-would override the default bindings (passing in an object meant to mimic the file 
-system in this example).  You can also override the default bindings with the class()
-constructor method, however the overrides would be ignored on subsequent calls (for
-that reason, it's probably best to only override bindings using the loadClass() method.
+In the first example, each call will return the same class instance (the 'default' 
+class instance).  In the second example, each call returns a new class instance.  The 
+second call also shows how you would override the default bindings (passing in an 
+object meant to mimic the file system in this example). 
+
+Class instances can be registered in a dictionary of named classes as follows:
+
+    var MyClassForTesting = require('./classfile').createClass('testing', {fs: mockFileSystem});
+    var MyRealClass = require('./classfile').createClass('real', {fs: require('fs')});
+
+You can later reference the same class instances as follows:
+
+    var MyClassForTesting = require('./classfile').class('testing');
+    var MyRealClass = require('./classfile').class('real');
+
+If the name is omitted, "default" is used.  The following expressions are equivalent:
+
+    var MyClass = require('./classfile').class();
+    var MyClass = require('./classfile').class('default');
 
 To create instances of your class, do the following:
 
     new MyClass();
 
-You can of course pass arguments to this constructor.
+For convenience, the "new" method is exported to allow instantiation:
+
+    var myInstance = require('./myClass').new();
+
+This form is equivalent to:
+
+    var MyClass = require('./myClass').class();
+    var myInstance = new MyClass();
+
+You may also instantiate instances of a specific class as follows:
+
+    var myTestInstance = require('./myClass').new('test');
 
 To create an inheritance chain, use the method inherit() as follows:
 
-    MyClass.inherit(MyOtherClass);
+    MyClass.inherit(MySuperClass);
+    MyClass.inherit(MySuper, MySuperSuper, MySuperDeDuper);
 
-To invoke a method from a superclass use supercall as follows:
+It's also possible to explicitly invoke methods of a superclass.  Invoke the superclass 
+constructor as follows:
 
     function Class() {
-      supercall(this, Class, arguments);
+      Class.super(this, arguments);
     };
 
-In a normal method (non constructor) do:
+Invoke any normal method in the superclass as follows:
 
     Class.prototype.methodA = function() {
-      supercall(this, Class, 'methodA', arguments);
+      Class.super(this, 'methodA', arguments);
     };
+
--- index.js
+++ index.js
@@ -1,54 +1,75 @@
 // Define functions and methods used for simulating class like structures
 // including inheritance.  An important feature of this system is that it
-// enabled one to define a class like structure while controlling all bindings
-// to the outside world (there is never a reason to make use "require()" directly
-// in a class definition...instead, bindings are passed into the class when the
-// class is loaded using loadClass().  Additionally, this system allows a class
-// to be instantiated multiple times, which enables a more compositional approach
-// to inheritance. 
+// enables one to define a class like structure while controlling all bindings
+// to the outside world.  This approach allows a class to be instantiated 
+// multiple times, which enables a more compositional approach to inheritance. 
 
-global.supercall = function(receiver, constructor, method, args) {
-  if(typeof method == 'string') {
-    constructor._super.prototype[method].apply(receiver, args);
-  } else {
-    //method is really args...used for calling the parent constructor
-    //on the receiver
-    constructor._super.apply(receiver, method);
-  }
-}
-
-module.__proto__.defineClass = function(constructor) {
-  //Load the class defined in the module.  If bindings are supplied, they override
-  //the default bindings supplied by the class definition.
-  this.exports.loadClass = function(bindings) {
-    var tmp = new constructor();
-    var answer = tmp.class;
+module.__proto__.defineClass = function(classConstructor) {
+  var self = this;
+  var classes = {};
+  
+  // Private class constructor
+  function _createClass(bindings) {
+    var answer = classConstructor(bindings || {});
     answer.inherit = function(parent) {
       if(arguments.length > 1) {
         // this allows chaining multiple classes in the call
         parent.inherit(Array.prototype.slice.call(arguments, 1));
-      }   
+      }
       this._super = parent;
       Object.defineProperty(this.prototype, '_constructor', {enumerable: false, value: this});
       this.prototype.__proto__ = parent.prototype;
       this.__proto__ = parent;
-    }
-    if(!bindings) bindings = {};
-    if(tmp.bindings) tmp.bindings(bindings, function() {
-      answer.inherit.apply(answer,arguments);
-    });
-    if(answer.initialize) answer.initialize();
+    };
+    answer.super = function(receiver, method, args) {
+      if(!this._super) return;
+      if(typeof method == 'string') {
+        return this._super.prototype[method].apply(receiver, args);
+      } else {
+        return this._super.apply(receiver, method);
+      }
+    };
+    if(answer.superclass) answer.inherit(answer.superclass);
     return answer;
   };
 
-  // createClass seems like a better name than loadClass
-  this.exports.createClass = this.exports.loadClass;
+  // Private class wrapper (we wrap classes to enable cyclic references)
+  function _wrapClass(wrapper, cls) {
+    wrapper.prototype = cls.prototype;
+    wrapper.prototype._constructor = wrapper;
+    wrapper._super = cls._super;
+    wrapper.inherit = cls.inherit;
+    wrapper.super = cls.super;
+    return wrapper;
+  };
 
-  //Return the class if it has already been constructed, if not, create
-  //it and set exports.theClass to the newly constructed class
-  this.exports.class = function(bindings) {
-    if(this.theClass) return this.theClass;
-    this.theClass = this.loadClass(bindings);
-    return this.theClass;
-  }
-}
+  // Public createClass() function - creates a new class with the given
+  // bindings and an optional name...the name, if given, can be used to
+  // later recall the same class instance using class())
+  this.exports.createClass = function(name, bindings) {
+    if(typeof name != 'string') return _createClass(bindings);
+    classes[name] = function() {return tmp.apply(this, arguments);};
+    var tmp = _createClass(bindings);
+    return _wrapClass(classes[name], tmp);
+  };
+
+  // Public class() function - Return the class for the given name if 
+  // it has already been created, otherwise create it (note, you cannot 
+  // override the default bindings with this method, use createClass() if
+  // you need to override the default bindings).  If the name is omitted, 
+  // use 'default'
+  this.exports.class = function(name) {
+    name = name || 'default';
+    if(classes[name]) return classes[name];
+    return this.createClass(name);
+  };
+
+  // Public new() method - This is a conventience function to create a 
+  // new instance of the "default" class instance
+  this.exports.new = function() {
+    var ClassInstance = this.class();
+    var answer = Object.create(ClassInstance.prototype);
+    ClassInstance.apply(answer, arguments);
+    return answer;
+  };
+};
